8de14a752a8fe27146461539ce7213518a80c1d5
“GodDamnGitHub” <jsczzzx@yahoo.com>
2026-01-26 11:53:47 -0500

    routing and refresh setup

--- apps/angular/src/app/home/home.component.ts
+++ apps/angular/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TestService } from '../test.service';
 
 @Component({
   selector: 'lmsweb-home',
@@ -8,4 +9,10 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  constructor(private testService: TestService) {}
+
+  ngOnInit() {
+    this.testService.print();
+  }
+}
