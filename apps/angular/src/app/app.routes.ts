e207340af2b1a64b6f57b94fdd956d9474cafb98
anguyen2002 <angnguyen2002@gmail.com>
2026-02-10 22:53:44 -0700

    signin skeleton (no logic) using angular materials)

--- apps/angular/src/app/app.routes.ts
+++ apps/angular/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Route } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 export const appRoutes: Route[] = [
   {
     path: '',
     loadChildren: () =>
       import('./remote-entry/entry.routes').then((m) => m.remoteRoutes),
+  },
+  {
+    path: 'sign-in', component: SignInComponent
   },
 ];
---
+++ apps/angular/src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'lmsweb-sign-in',
+  standalone: true,
+  imports: [
+    CommonModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatIconModule,
+    MatButtonModule,
+    HttpClientModule
+  ],
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css'],
+})
+export class SignInComponent {}
