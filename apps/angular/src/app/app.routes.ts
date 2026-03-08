0239610b024daba7784c38db3b0f44aba4540444
unknown <mazidatou99@gmail.com>
2026-03-08 04:24:43 -0500

    first commit

--- apps/angular/src/app/app.routes.ts
+++ apps/angular/src/app/app.routes.ts
@@ -3,11 +3,8 @@ import { SignInComponent } from './features/sign-in/sign-in.component';
 import { LandingComponent } from './features/landing/landing.component';
 import { EmailVerificationComponent } from './features/emailVerification/emailVerification.component';
 import { RegisterComponent } from './features/register/register.component';
-<<<<<<< HEAD
-import { ForgotPasswordComponent } from './features/forgot-password/forgotPassword.component';
-=======
 import { EmailConfirmationComponent } from './features/emailVerification/email-confirmation/email-confirmation.component';
->>>>>>> origin/angular
+import { ForgotPasswordComponent } from './features/forgot-password/forgotPassword.component';
 
 export const appRoutes: Route[] = [
   {
@@ -26,16 +23,11 @@ export const appRoutes: Route[] = [
 
   },
   {
-<<<<<<< HEAD
-    path:'register', component: RegisterComponent
-  },
-  {
-    path: 'forgotPassword', component: ForgotPasswordComponent
-=======
     path: 'register', component: RegisterComponent
   },
   {
     path: 'confirmation', component: EmailConfirmationComponent
->>>>>>> origin/angular
+  },{
+    path:'forgotPassword', component: ForgotPasswordComponent
   }
 ];
